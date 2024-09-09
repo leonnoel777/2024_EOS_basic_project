09780ebf7a5b5429002d53eca7cc3eb0d3312094
com.kimmoohyun <leonnoel777@naver.com>
2024-09-09 17:53:38 +0900

    hello world

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '김무현 앱',
+      title: 'eos_todolist',
       theme: ThemeData(
         // This is the theme of your application.
         //
