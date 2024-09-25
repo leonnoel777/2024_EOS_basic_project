c025e013e7beb52294df21a6eae42aa2098269b5
com.kimmoohyun <leonnoel777@naver.com>
2024-09-25 16:46:23 +0900

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'EOS ToDoList',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
+        fontFamily: 'Pretendard',
         useMaterial3: true,
       ),
       home: const MyHomePage(),
@@ -27,86 +28,56 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.lightGreen,
-        title: Text('EOS ToDoList'),
-        leading: Icon(Icons.check_box_outlined),
+        backgroundColor: Color(0xFFA4C639).withOpacity(0.1),
+        centerTitle: false,
+        title: const Text('EOS ToDoList'),
+        leading: Image.asset('assets/images/eos_logo.png'),
       ),
-      body: Container(
-        height: 200,
-        color: Colors.lightGreen.withOpacity(0.3),
-        padding: EdgeInsets.all(25),
-        child: Row(
-          children: [
-          Container (
-            width: 150,
-            height: 150,
-            decoration: BoxDecoration (
-              color: Colors.white,
-              border: Border.all(width: 10, color: Colors.grey),
-            ),
-            child: Center(
-              child: Icon(
-                Icons.person,
-                size: 100,
-                color: Colors.grey,
-            )),
-          ),
-          SizedBox(
-            width: 10,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('EOS'),
-              Text(
-                '김무현',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            padding: const EdgeInsets.all(25),
+            child: Row(
+              children: [
+                Container(
+                  width: 140,
+                  height: 140,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(width: 10, color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8070),
+                  ),
+                  child: Center(
+                    child: Image.asset('assets/images/eos_logo.png'),
+                  ),
                 ),
-              ),
-              Text('과제다했다~~')
-            ])
-          ])));
-  }
-}
-/* class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Hi'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+                const SizedBox(
+                  width: 35,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      '김무현',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text('나를 소개하는 한마디!'),
+                  ],
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+          Container(
+            height: 500,
+            color: Color(0xFFA4C639).withOpacity(0.1),
+          ),
+        ],
       ),
     );
   }
 }
-*/
